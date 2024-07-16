24ce877cf0ad98e1792ee6e4968c5daaa47a4d39
linsir1984 <linsir84@gmail.com>
2024-07-16 11:03:14 +0900

    add comments to user configuration

--- src/ASUCloud/ASUCloud.Repository/UserConfiguration.cs
+++ src/ASUCloud/ASUCloud.Repository/UserConfiguration.cs
@@ -8,6 +8,11 @@ using System.Threading.Tasks;
 
 namespace ASUCloud.Repository
 {
+    /// <summary>
+    /// UserConfiguration
+    /// この設定はsqlite用であり、
+    /// 他のデータベースへの変更はデータ型を変更する必要がある。
+    /// </summary>
     public class UserConfiguration : IEntityTypeConfiguration<User>
     {
         public void Configure(EntityTypeBuilder<User> builder)
@@ -58,7 +63,9 @@ namespace ASUCloud.Repository
 
             builder.HasIndex(u => u.ID, "primary_key");
 
-            builder.HasIndex(u=>new {u.Name, u.Email}, "name_email_avoid_dup");
+            //重複ユーザー、つまり同じ名前で同じEメールを持つユーザーの追加を避けるため、データベースのname,emailフィールドにユニークなインデックスが追加されます。
+            builder.HasIndex(u=>new {u.Name, u.Email}, "name_email_avoid_dup")
+                .IsUnique();
 
         }
     }
